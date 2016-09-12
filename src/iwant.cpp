36bca4dd5c1de20b68bc0c5d44175b465f2ef121
haochenprophet <hao.chen@prophet.net.cn>
2016-09-12 17:09:13 +0800

    1.init iwant coding

---
+++ src/iwant.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "iwant.h"
+#define IWANT_TEST	1
+
+
+Ciwant::Ciwant()
+{
+#if IWANT_TEST	
+	printf("Ciwant::Ciwant()\n");
+#ednif	
+}
+
+Ciwant::~Ciwant()
+{
+#if IWANT_TEST
+	printf("Ciwant::~Ciwant()\n");
+#ednif	
+}
+
+#if IWANT_TEST
+int main(int argc ,char *argv[])
+{
+	Ciwant i;
+	
+	return 0;
+}
+#ednif
---
+++ src/iwant.h
@@ -0,0 +1,11 @@
+#ifndef IWANT_H
+#define IWANT_H
+
+class Ciwant
+{
+public:
+		Ciwant();
+		~Ciwant();
+};
+
+#endif
