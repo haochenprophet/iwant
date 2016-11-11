2732c49b222a584dd1d1eb6a9b8d3cf905d92c64
haochenprophet <hao.chen@prophet.net.cn>
2016-11-11 11:48:38 +0800

    1.Updtae build-sample-code-math-makefile.bat for add clean: label to Makefile for math.

--- batch/build-sample-code-math-makefile.bat
+++ batch/build-sample-code-math-makefile.bat
@@ -11,5 +11,11 @@ echo.>>..\%WK_PATH%\Makefile
 
 for /F " eol=; delims=." %%s in (dir.log) do @call build-make.bat %%s   ..\%WK_PATH%\Makefile
 
+
+echo clean:>>..\%WK_PATH%\Makefile
+echo 	rm -f fstream *.o *~* *.txt *.out \>>..\%WK_PATH%\Makefile
+for /F " eol=; delims=." %%s in (dir.log) do @echo %%s \>>..\%WK_PATH%\Makefile
+echo.>>..\%WK_PATH%\Makefile
+
 @if exist dir.log del /F dir.log
 ::@pause
