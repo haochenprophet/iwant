20c0f1de65ce4ca35675049960edaa5804e81772
chenhaohaochenlulu <xhfch@126.com>
2016-11-27 18:29:44 +0800

    1.Add install-lamp-server.sh , release-websit.sh ,Update test.php .

---
+++ website/release-websit.sh
@@ -0,0 +1,1 @@
+sudo cp -r -f ./ /var/www/html/
---
+++ website/test.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>PHP-test</title>
+	</head>
+
+	<body>
+		<?php 
+			echo "<p>PHP-test</p>";
+			echo "<p>Hello World</p>";
+		?>
+	</body>
+</html>
