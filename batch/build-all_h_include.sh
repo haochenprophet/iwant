8e20891b321d8e136dd5f696fa51c97045d78ed6
hunter <hao__chen@sina.com>
2016-12-08 11:28:58 +0800

    Update build-all_h_include.sh ,src/all_h_include.h of linux .

--- batch/build-all_h_include.sh
+++ batch/build-all_h_include.sh
@@ -4,10 +4,12 @@ echo \#ifndef ALL_H_INCLUDE > all_h_include.h
 echo \#define ALL_H_INCLUDE >>all_h_include.h
 echo >>all_h_include.h
 
-for i in $(ls *.h)
+for i in $(ls *.h sql/*.h)
 do
 	echo \#include \"$i\" >>all_h_include.h
 done
 
 echo >>all_h_include.h
 echo \#endif >>all_h_include.h
+
+make
