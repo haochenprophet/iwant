fb1fca680a0b7efa0ae1b16a064a466e85f83c0a
chenhaohaochenlulu <xhfch@126.com>
2017-03-03 22:52:43 +0800

    Add batch/install-chrome.sh ,sample-code/sql/Makefile

---
+++ batch/install-chrome.sh
@@ -0,0 +1,7 @@
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+ls /opt/google/chrome/
+
+
