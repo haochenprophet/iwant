d40b50b6e642d71f9b64923eb09eae77b33c25cd
haochenprophet <hao.chen@prophet.net.cn>
2017-10-19 03:56:57 +0800

    Add batch/build_instructions_keyword.sh

---
+++ batch/build_instructions_keyword.sh
@@ -0,0 +1,667 @@
+make main=CLASS
+./iwant 
+./iwant AAA
+./iwant AAD
+./iwant AAM
+./iwant AAS
+./iwant ADC
+./iwant ADCX
+./iwant ADD
+./iwant ADDPD
+./iwant ADDPS
+./iwant ADDSD
+./iwant ADDSS
+./iwant ADDSUBPD
+./iwant ADDSUBPS
+./iwant ADOX
+./iwant AESDEC
+./iwant AESDECLAST
+./iwant AESENC
+./iwant AESENCLAST
+./iwant AESIMC
+./iwant AESKEYGENASSIST
+./iwant AND
+./iwant ANDN
+./iwant ANDPD
+./iwant ANDPS
+./iwant ANDNPD
+./iwant ANDNPS
+./iwant ARPL
+./iwant BLENDPD
+./iwant BEXTR
+./iwant BLENDPS
+./iwant BLENDVPD
+./iwant BLENDVPS
+./iwant BLSI
+./iwant BLSMSK
+./iwant BLSR 
+./iwant BNDCL
+./iwant BNDCU
+./iwant BNDCN
+./iwant BNDLDX
+./iwant BNDMK
+./iwant BNDMOV
+./iwant BNDSTX
+./iwant BOUND
+./iwant BSF
+./iwant BSR
+./iwant BSWAP
+./iwant BT
+./iwant BTC
+./iwant BTR
+./iwant BTS
+./iwant BZHI
+./iwant CALL
+./iwant CBW
+./iwant CWDE
+./iwant CDQE
+./iwant CLAC
+./iwant CLC
+./iwant CLD
+./iwant CLFLUSH
+./iwant CLFLUSHOPT
+./iwant CLI
+./iwant CLTS
+./iwant CLWB
+./iwant CMC
+./iwant CMOVcc
+./iwant CMP
+./iwant CMPPD
+./iwant CMPPS
+./iwant CMPS
+./iwant CMPSB
+./iwant CMPSW
+./iwant CMPSD
+./iwant CMPSQ
+./iwant CMPSD
+./iwant CMPSS
+./iwant CMPXCHG
+./iwant CMPXCHG8B
+./iwant CMPXCHG16B
+./iwant COMISD
+./iwant COMISS
+./iwant CPUID
+./iwant CRC32
+./iwant CVTDQ2PD
+./iwant CVTDQ2PS
+./iwant CVTPD2DQ
+./iwant CVTPD2PI
+./iwant CVTPD2PS
+./iwant CVTPI2PD
+./iwant CVTPI2PS
+./iwant CVTPS2DQ
+./iwant CVTPS2PD
+./iwant CVTPS2PI
+./iwant CVTSD2SI
+./iwant CVTSD2SS
+./iwant CVTSI2SD
+./iwant CVTSI2SS
+./iwant CVTSS2SD
+./iwant CVTSS2SI
+./iwant CVTTPD2DQ
+./iwant CVTTPD2PI
+./iwant CVTTPS2DQ
+./iwant CVTTPS2PI
+./iwant CVTTSD2SI 
+./iwant CVTTSS2SI
+./iwant CWD
+./iwant CDQ 
+./iwant CQO
+./iwant DAA 
+./iwant DAS
+./iwant DEC
+./iwant DIV
+./iwant DIVPD
+./iwant DIVPS
+./iwant DIVSD
+./iwant DIVSS
+./iwant DPPD
+./iwant DPPS
+./iwant EMMS
+./iwant ENTER
+./iwant EXTRACTPS
+./iwant F2XM1
+./iwant FABS
+./iwant FADD
+./iwant FADDP
+./iwant FIADD
+./iwant FBLD
+./iwant FBSTP
+./iwant FCHS
+./iwant FCLEX
+./iwant FNCLEX
+./iwant FCMOVcc
+./iwant FCOM
+./iwant FCOMP
+./iwant FCOMPP
+./iwant FCOMI
+./iwant FCOMIP
+./iwant FUCOMI
+./iwant FUCOMIP
+./iwant FCOS
+./iwant FDECSTP
+./iwant FDIV
+./iwant FDIVP
+./iwant FIDIV
+./iwant FDIVR
+./iwant FDIVRP
+./iwant FIDIVR
+./iwant FFREE
+./iwant FICOM
+./iwant FICOMP
+./iwant FILD
+./iwant FINCSTP
+./iwant FINIT
+./iwant FNINIT
+./iwant FIST
+./iwant FISTP
+./iwant FLD
+./iwant FLD1
+./iwant FLDL2T
+./iwant FLDL2E
+./iwant FLDPI
+./iwant FLDLG2
+./iwant FLDLN2
+./iwant FLDZ
+./iwant FLDCW
+./iwant FLDENV
+./iwant FMUL
+./iwant FMULP
+./iwant FIMUL
+./iwant FNOP
+./iwant FPATAN
+./iwant FPREM
+./iwant FPREM1
+./iwant FPTAN
+./iwant FRNDINT
+./iwant FRSTOR
+./iwant FSAVE
+./iwant FNSAVE
+./iwant FSCALE
+./iwant FSIN
+./iwant FSINCOS
+./iwant FSQRT
+./iwant FST
+./iwant FSTP
+./iwant FSTCW
+./iwant FNSTCW
+./iwant FSTENV
+./iwant FNSTENV
+./iwant FSTSW
+./iwant FNSTSW
+./iwant FSUB
+./iwant FSUBP
+./iwant FISUB
+./iwant FSUBR
+./iwant FSUBRP
+./iwant FISUBR
+./iwant FTST
+./iwant FUCOM
+./iwant FUCOMP
+./iwant FUCOMPP
+./iwant FXAM
+./iwant FXCH
+./iwant FXRSTOR
+./iwant FXSAVE
+./iwant FXTRACT
+./iwant FYL2X
+./iwant FYL2XP1
+./iwant HADDPD
+./iwant HADDPS
+./iwant HLT
+./iwant HSUBPD
+./iwant HSUBPS
+./iwant IDIV
+./iwant IMUL
+./iwant IN
+./iwant INC
+./iwant INS
+./iwant INSB
+./iwant INSW
+./iwant INSD
+./iwant INSERTPS
+./iwant INT
+./iwant INTO
+./iwant INT_3
+./iwant INVD
+./iwant INVLPG
+./iwant INVPCID
+./iwant IRET
+./iwant IRETD
+./iwant JCC
+./iwant JMP
+./iwant KADDW
+./iwant KADDB
+./iwant KADDQ
+./iwant KADDD
+./iwant KANDW
+./iwant KANDB
+./iwant KANDQ
+./iwant KANDD
+./iwant KANDNW
+./iwant KANDNB
+./iwant KANDNQ
+./iwant KANDND
+./iwant KMOVW
+./iwant KMOVB
+./iwant KMOVQ
+./iwant KMOVD
+./iwant KNOTW
+./iwant KNOTB
+./iwant KNOTQ
+./iwant KNOTD
+./iwant KORW
+./iwant KORB
+./iwant KORQ
+./iwant KORD
+./iwant KORTESTW
+./iwant KORTESTB
+./iwant KORTESTQ
+./iwant KORTESTD
+./iwant KSHIFTLW
+./iwant KSHIFTLB
+./iwant KSHIFTLQ
+./iwant KSHIFTLD
+./iwant KSHIFTRW
+./iwant KSHIFTRB
+./iwant KSHIFTRQ
+./iwant KSHIFTRD
+./iwant KTESTW
+./iwant KTESTB
+./iwant KTESTQ
+./iwant KTESTD
+./iwant KUNPCKBW
+./iwant KUNPCKWD
+./iwant KUNPCKDQ
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+./iwant 
+
+make main=CLASS
