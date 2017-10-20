54c95747f9022766026523e8d501c2035e5da152
hunter <hao__chen@sina.com>
2017-10-20 09:56:53 +0800

    Add class Cinstruction

---
+++ src/instruction.h
@@ -0,0 +1,18 @@
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+#include "object.h"
+
+namespace n_instruction {
+	class Cinstruction :public Object
+	{
+	public:
+		Cinstruction();
+		~Cinstruction();
+		int my_init(void *p=nullptr);
+	};
+}
+
+using namespace n_instruction;
+
+#endif
