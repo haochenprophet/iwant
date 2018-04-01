db941684f3502074d2a22bcd0bb10ecbaa7e1b7e
chenhaohaochenlulu <xhfch@126.com>
2018-04-01 19:08:15 +0800

    modified:   sample-code/sql/mysql_real_connect/test.cpp

--- sample-code/sql/mysql_real_connect/test.cpp
+++ sample-code/sql/mysql_real_connect/test.cpp
@@ -4,11 +4,13 @@
 
 int main()
 {
+  int ret=0;
   MYSQL mysql;
   char * host=(char *)"127.0.0.1";
   char * user=(char *)"root";
   char * password=(char *)"password";
   char * use_db=(char *)"test";
+
 
   mysql_init(&mysql);
   mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"test");
@@ -16,8 +18,9 @@ int main()
   if (!mysql_real_connect(&mysql,host,user,password,use_db,0,NULL,0))
   {
       fprintf(stderr, "Failed mysql_real_connect: %s\n",mysql_error(&mysql));
+      return ++ret;
   }
   fprintf(stdout, "mysql_real_connect pass!\n");
   mysql_close(mysql);
-  return 0;
+  return ret;
 }
