833eb09a4110651637574fae0af63bc9a39e779e
chenhaohaochenlulu <xhfch@126.com>
2018-04-01 19:03:43 +0800

    Add sample-code/sql/mysql_real_connect/

---
+++ batch/mysql/init-mysql.sh
@@ -0,0 +1,5 @@
+#sudo apt-get install mysql-server
+sudo apt-get install mysql-client
+sudo apt-get install gcc g++ libgcc1 libg++ make gdb
+sudo apt-get install libmysql*
+sudo cp /usr/lib/mysql/* /usr/lib/
---
+++ sample-code/sql/mysql_real_connect/test.cpp
@@ -0,0 +1,23 @@
+#include <mysql/mysql.h>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+  MYSQL mysql;
+  char * host=(char *)"127.0.0.1";
+  char * user=(char *)"root";
+  char * password=(char *)"password";
+  char * use_db=(char *)"test";
+
+  mysql_init(&mysql);
+  mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"test");
+
+  if (!mysql_real_connect(&mysql,host,user,password,use_db,0,NULL,0))
+  {
+      fprintf(stderr, "Failed mysql_real_connect: %s\n",mysql_error(&mysql));
+  }
+  fprintf(stdout, "mysql_real_connect pass!\n");
+  mysql_close(mysql);
+  return 0;
+}
