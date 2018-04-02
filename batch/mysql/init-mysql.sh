21e617772f7638c12558b264f673d8e4ebe4b968
hunter <hao__chen@sina.com>
2018-04-02 17:03:18 +0800

    modified:   batch/mysql/init-mysql.sh

--- batch/mysql/init-mysql.sh
+++ batch/mysql/init-mysql.sh
@@ -1,5 +1,6 @@
-#sudo apt-get install mysql-server
+sudo apt-get install mysql-server
 sudo apt-get install mysql-client
 sudo apt-get install gcc g++ libgcc1 libg++ make gdb
 sudo apt-get install libmysql*
+sudo apt-get install libmysqlclient-dev
 sudo cp /usr/lib/mysql/* /usr/lib/
