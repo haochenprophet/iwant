892249c8d04ea1403558c89fe70214bff84ebb93
haochenprophet <hao.chen@prophet.net.cn>
2018-04-03 09:21:11 +0800

    Update class Cquestion

--- src/question.cpp
+++ src/question.cpp
@@ -1,42 +1,213 @@
 #include "question.h"
+
+int Cquestion::q_count=0;
+int Cquestion::q_value=0;
+int Cquestion::q_direction=0;
+int Cquestion::loop_count=1;//question status and loop
+
 int Cquestion::my_init(void *p)
 {
 	this->name = "Cquestion";
 	this->alias = "question";
+
 	return 0;
 }
 
 Cquestion::Cquestion()
 {
 	this->my_init();
+	this->q_buf_len=this->allot(Q_BUF_LEN,(void **)&this->q_buf);
+	this->a_buf_len=this->allot(Q_BUF_LEN,(void **)&this->a_buf);
+	this->r_buf_len=this->allot(Q_BUF_LEN,(void **)&this->r_buf);
+	this->f_buf_len=this->allot(Q_BUF_LEN,(void **)&this->f_buf);
+	this->d_buf_len=this->allot(Q_BUF_LEN,(void **)&this->d_buf);
+}
+
+Cquestion::~Cquestion()
+{
+	if(this->q_buf) delete[] this->q_buf;
+	if(this->a_buf) delete[] this->a_buf;
+	if(this->r_buf) delete[] this->r_buf;
+	if(this->f_buf) delete[] this->f_buf;
+	if(this->d_buf) delete[] this->d_buf;
 }
 
 
-int Cquestion::q_question(char * sQ,char *sA ,char *sR)
+int Cquestion::q_dir_value(char *sR,char* fB,int abcd)
 {
-	int ret=0;
 	char *cp;
-	cout<<++this->count<<": "<<sQ<<endl;
-	cout<<sA<<endl;
-	ret=wait_char_abcd(nullptr);
-
-	cp=this->get_result((char*)sR,ret,1);
+	cp=this->get_result((char*)sR,abcd,1);
 	if(cp) 
 	{
 		//cout<<cp<<endl;cout<<atoi(cp)<<endl;//test ok
-		this->direction+=atoi(cp);
+		Cquestion::q_direction+=atoi(cp);
+		if(fB)
+		{
+				cp=this->get_feedback((char*)fB,abcd,1);
+				if(cp) Cquestion::q_direction+=atoi(cp);
+		}
 	}
 	
-	cp=this->get_result((char*)sR,ret,2);
+	cp=this->get_result((char*)sR,abcd,2);
 	if(cp)
 	{
 		 //cout<<cp<<endl;cout<<atoi(cp)<<endl;//test ok
-		 this->value+=atoi(cp);
+		 Cquestion::q_value+=atoi(cp);
+		if(fB)
+		{
+				cp=this->get_feedback((char*)fB,abcd,2);
+				if(cp) Cquestion::q_value+=atoi(cp);
+		}
 	}
-
-	return ret;//abcd
+	return 0;
 }
 
+int Cquestion::q_type(int type,char *sR,char * fB,int action)
+{
+	Uqresult ret;
+	ret.i_abcd=0;
+	
+	if(type==Q_SWITCH&&action==Q_WAIT_CHAR)
+	{
+		ret.i_abcd=wait_char_abcd(NULL);
+		this->q_dir_value(sR,fB,ret.i_abcd);
+	}
+
+	if(type==Q_CHOICE)
+	{
+		if(action==Q_WAIT_CHAR)
+		{
+			ret.i_abcd=wait_char_abcd(NULL);
+			this->q_dir_value(sR,fB,ret.i_abcd);	
+		}
+		
+		if(action==Q_WAIT_STR)
+		{
+			cout<<(char *)MULTIPLE_CHOICE;
+			this->wait_cin();
+			if(this->at_cin_buf((char*)"A")||this->at_cin_buf((char*)"a"))
+			{
+				ret.c_abcd[0]='A';
+				this->q_dir_value(sR,fB,1);
+			}
+
+			if(this->at_cin_buf((char*)"B")||this->at_cin_buf((char*)"b"))
+			{
+				ret.c_abcd[1]='B';
+				this->q_dir_value(sR,fB,2);
+			}
+			
+			if(this->at_cin_buf((char*)"C")||this->at_cin_buf((char*)"c"))
+			{
+				ret.c_abcd[2]='C';
+				this->q_dir_value(sR,fB,3);
+			}
+
+			if(this->at_cin_buf((char*)"D")||this->at_cin_buf((char*)"d"))
+			{
+				ret.c_abcd[3]='D';
+				this->q_dir_value(sR,fB,4);
+			}
+		}
+	}
+
+	return ret.i_abcd;
+}
+
+
+char * Cquestion::get_depend(char * sD,int abcd,int type)
+{
+	char s[]="X";
+	if(type==Q_SWITCH)
+	{
+		switch(abcd)
+		{
+			case 1:
+				s[0]='A';
+				break;
+			case 2:
+				s[0]='B';
+				break;
+			case 3:
+				s[0]='C';
+				break;
+			case 4:
+				s[0]='D';
+				break;
+			default:
+				return nullptr;
+		}
+		return strstr(sD,s);
+	}
+
+	if(type==Q_CHOICE)//parse Q_CHOICE 'A' 'B' 'C' 'D' 
+	{
+		Uqresult r;
+		r.i_abcd=abcd;
+		char *cp;
+
+		for(int n=0;n<Q_SWITCH_LEN;n++)
+		{
+			if(r.c_abcd[n]=='\0') continue;
+			s[0]=r.c_abcd[n];
+			cp=strstr(sD,s);
+			if(cp) return cp;
+		}
+	}
+
+	return nullptr;
+}
+
+int Cquestion::q_question(char * sQ,char *sA ,char *sR,char* fB,int type,int action,char *log)
+{
+	Uqresult ret;
+	ret.i_abcd=0;
+
+	if(!(sQ&&sA&&sR)) return 0;//checkt nullptr
+
+	cout<<++Cquestion::q_count<<": "<<sQ<<endl;
+	cout<<sA<<endl;
+
+	ret.i_abcd=this->q_type(type,sR,fB,action);
+	
+	cout<<endl;
+
+	if(log&&this->q_buf)
+	{
+		static Cfile file;
+		sprintf(this->q_buf, "%d:%s\n%s\n[%d]\n",Cquestion::q_count,sQ,sA,ret.i_abcd);
+		file.f_append(log,this->q_buf);
+	}
+
+	return ret.i_abcd;//abcd
+}
+
+int Cquestion::q_question(Squestion *q,char *log)
+{
+	char *sD;// depend condition
+	int n,d;
+	for(n=0;q[n].sQ!=nullptr;n++)
+	{
+		if(q[n].status==Cquestion::loop_count) continue; //had exec this question
+
+		if(q[n].depend!=0)//check depend
+		{
+			d=q[n].depend;
+			if(q[n+d].status!=Cquestion::loop_count&&!q[n+d].sD)//check not run
+			{
+				if(q[n+d].sQ==nullptr) continue;
+
+				q[n+d].result=this->q_question(q[n+d].sQ,q[n+d].sA,q[n+d].sR,q[n+d].sF,q[n+d].type,q[n+d].action,log);
+				q[n+d].status=Cquestion::loop_count;				
+			}
+			if(!this->get_depend(q[n].sD,q[n+d].result,q[n+d].type)) continue;
+		}
+
+		q[n].result=this->q_question(q[n].sQ,q[n].sA,q[n].sR,q[n].sF,q[n].type,q[n].action,log);
+		q[n].status=Cquestion::loop_count;
+	}
+	return 0;
+}
 
 char * Cquestion::get_result(char *strR,int abcd,int index)
 {
@@ -80,3 +251,27 @@ char * Cquestion::get_result(char *strR,int abcd,int index)
 	}
 	return nullptr;
 }
+
+char * Cquestion::get_feedback(char *strF,int abcd,int index)
+{
+	return this->get_result(strF,abcd,index);
+}
+
+int Cquestion::question(void * p)
+{
+	return 0;
+}
+
+#ifndef QUESTION_TEST
+#define QUESTION_TEST 0//1
+#endif
+
+#if QUESTION_TEST
+#include "all_h_include.h"
+int main(int argc, char *argv[])
+{
+	Coperate o;
+	o.question((void *)"question.log");
+	return 0;
+}
+#endif 
--- src/question.h
+++ src/question.h
@@ -1,17 +1,78 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 
+enum E_QTYPE{
+	Q_SWITCH,//default:  Q switch esult A ,B,C,D /
+	Q_CHOICE, //Q choice like ABC,AB ,BC ABCD
+	Q_STRING, //resutl string ,like text
+	Q_EXPR,//expression 
+	Q_CODE,//
+	Q_FILE,
+	Q_IMAGE,
+};
+
+enum E_QACTION{
+	Q_WAIT_CHAR,//default: wait char
+	Q_WAIT_STR,//wait str
+};
+
+typedef struct Squestion_T {
+	int id;//>0 ==0 no define
+	int type;//ABCD ,some ABCD ,string
+	char *sQ;//question
+	char *sA;//answer
+	char *sR;//result
+	char *sF;//result feedback
+	char *sD;// depend condition
+	int depend; //depend index befor:-n ,after:=n, or for depend id >0
+	int result; //result of choice
+	int status;//not run =0 ;run=1
+	int action;
+}Squestion;
+
+/* //sample code
+Squestion_T q_sample[]
+{
+	{sQ,sA,sR,sF,sD,depend,choice,status,E_QTYPE,id}
+	{nullptr,nullptr,nullptr,nullptr,nullptr,0,0,0,0,0},
+};
+*/
+#define Q_SWITCH_LEN 4
+typedef union Uqresult_T
+{  
+        char c_abcd[Q_SWITCH_LEN];  
+        int  i_abcd;  
+}Uqresult;
+
 #include "object.h"
+#include "file.h"
 #include "question_def.h"
+
+#define Q_BUF_LEN O_BUF_LEN
 
 namespace n_question {
 	class Cquestion :public Object
 	{
 	public:
+		char * q_buf,*a_buf,*r_buf,*f_buf,*d_buf;
+		int q_buf_len,a_buf_len,r_buf_len,f_buf_len,d_buf_len;
+	public:
+		static int q_count;
+		static int q_value;
+		static int q_direction;
+		static int loop_count;
+	public:
 		Cquestion();
+		~Cquestion();
 		int my_init(void *p=nullptr);
-		int q_question(char * sQ,char *sA ,char *sR);//return abcd
+		int q_type(int type,char *sR,char * fB,int action=Q_WAIT_CHAR);
+		int q_dir_value(char *sR,char* fB,int abcd);
+		int q_question(char * sQ,char *sA ,char *sR,char* fB=nullptr,int type=Q_SWITCH,int action=Q_WAIT_CHAR,char *log=nullptr);//return abcd
+		int q_question(Squestion *q,char *log=nullptr);
+		char * get_depend(char * sD,int abcd,int type=Q_SWITCH);
 		char * get_result(char *strR,int abcd,int index);
+		char * get_feedback(char *strF,int abcd,int index);
+		int question(void * p = NULL);
 	};
 }
 
