b81a989b7471c2e98e085de5a359a97257d72599
haochenprophet <hao__chen@sina.com>
2021-05-14 17:37:47 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+src_dir=$PWD
+date
+echo "Start build:$src_dir"
+#step 1:check and rm
+if [[ -f ${src_dir}/src/iwant ]];then
+	rm ${src_dir}/src/iwant -f
+fi
+#step 2:make
+cd ${src_dir}/src/
+make
+#step 3:check and mv
+if [[ -f ${src_dir}/src/iwant ]];then
+	mv ${src_dir}/src/iwant ${src_dir}/ -f
+	echo "Build done!"
+else
+	echo "Build fail!"
+fi
+#
+date
