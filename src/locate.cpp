1987a68f3e50cb06bdbf751dbabfb80f40c1017a
hunter <hao__chen@sina.com>
2022-11-23 11:07:09 +0800

    Add Clocate::line()

--- src/locate.cpp
+++ src/locate.cpp
@@ -15,6 +15,38 @@ Clocate::~Clocate()
 {
 
 }
+//\r=13 \n=10 : windows \r\n  linux:\n  macos :\r
+uint8_t* Clocate::line(uint8_t* start, uint8_t* end, int64_t line_number)
+{
+	int64_t line;
+
+	for (line=1; start < end; start++)
+	{
+		if (line == line_number) break; //n p
+
+		if (*start == '\n')//linux style
+		{
+			line++;
+			continue;
+		}
+
+		if (*start == '\r')//windows or macos style
+		{
+			line++;
+			if (start + 1 < end && start[1] == '\n') //windows style
+			{
+				start ++;//point to '\n' for() start++ will skip \n
+			}
+		}
+	}
+
+	return start;
+}
+
+uint8_t* Clocate::line(uint8_t* start, size_t size, int64_t line_number)
+{
+	return this->line(start, start + size, line_number);
+}
 
 #ifndef LOCATE_TEST
 #define LOCATE_TEST 0//1
--- src/locate.h
+++ src/locate.h
@@ -10,6 +10,8 @@ namespace n_locate {
 		Clocate();
 		~Clocate();
 		int my_init(void *p=nullptr);
+		uint8_t* line(uint8_t* start, uint8_t* end, int64_t line_number);
+		uint8_t* line(uint8_t* start, size_t size, int64_t line_number);
 	};
 }
 
