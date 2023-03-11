f3c4bc84ad22385b43800ecc4d18cab8930792a1
hunter <hao__chen@sina.com>
2023-03-11 19:23:28 +0800

    Update ample-code/linux/driver/mmio

--- sample-code/linux/driver/mmio/mmio.c
+++ sample-code/linux/driver/mmio/mmio.c
@@ -10,31 +10,46 @@
 #include <linux/uaccess.h>
 #include "mmio.h"
 
-static int mmio_open(struct inode *inode, struct file *filp)
+mmio_action action;//for read() write()
+
+mmio_map_range map_range={0,0};
+
+int check_address_map_range(physical_address phy_addr)
 {
+
+	return 0;
+}
+
+static int mmio_open(struct inode *inode, struct file *filp)
+{  
 	AT_LINE
 	return 0;
 }
 
 static ssize_t mmio_read (struct file *file, char __user *buf, size_t size, loff_t *offset)
 {
-		AT_LINE
+	int ret;	AT_LINE
+	ret = copy_from_user(&action, buf, sizeof(mmio_action));
+	ret = copy_to_user(buf, &action, sizeof(mmio_action));
 	return 0;
 }
 
 static ssize_t mmio_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
-{
-		AT_LINE
-    return 0;
+{  
+
+	int ret;AT_LINE
+	ret = copy_from_user(&action, buf, sizeof(mmio_action));
+	ret = copy_to_user((char *)buf,&action, sizeof(mmio_action));
+   return 0;
 }
 
+//create file device 
 static struct file_operations mmio_fops = {
    .owner = THIS_MODULE,
    .open = mmio_open,
    .read = mmio_read,
    .write = mmio_write,
 };
-
 
 int mmio_device;
 struct class *mmio_class;
@@ -62,3 +77,4 @@ module_exit(mmio_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("https://github.com/haochenprophet");
+MODULE_DESCRIPTION("Device Memory Map Input Output Address Access");
--- sample-code/linux/driver/mmio/mmio.h
+++ sample-code/linux/driver/mmio/mmio.h
@@ -1,6 +1,7 @@
 #ifndef MMIO_H
 #define MMIO_H  
 
+//debug code 
 #ifndef LINUX_DEBUG
 #define LINUX_DEBUG  1
 #endif
@@ -11,5 +12,31 @@
     #define AT_LINE  
 #endif
 
+//mmio code 
+#define MMIO_MAP_SIZE  (4096) //one address map 4KB(0x1000) one page for mmio_action access
+
+typedef unsigned long long physical_address; //64 bit address 
+typedef unsigned long long mmio_data; //64 bit data
+
+enum mmio_action_enum { 
+    mmio_byte_read=1, mmio_word_read,  mmio_dword_read, mmio_qword_read, // read action : [mmio_address] to data 
+    mmio_byte_write,  mmio_word_write, mmio_dword_write,mmio_qword_write,// write action: data to [mmio_address] 
+    mmio_byte_and,    mmio_word_and,   mmio_dword_and,  mmio_qword_and,// write and action: data to [mmio_address] 
+    mmio_byte_or,     mmio_word_or,    mmio_dword_or,   mmio_qword_or,// write or action: data to [mmio_address] 
+};
+
+typedef struct mmio_action_struct
+{
+    physical_address phy_addr;
+    mmio_data data;
+    int action;
+    int status;//0 :success >0 fail <0 do nothing
+}mmio_action;
+
+typedef struct mmio_map_range_struct
+{
+    physical_address phy_addr;//4K alignment
+    size_t size; //4K units
+}mmio_map_range;
 
 #endif //MMIO_H  
