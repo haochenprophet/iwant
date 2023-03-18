6e90efb4362afcfce79b6cdfeca12a2a867dffd7
hunter <hao__chen@sina.com>
2023-03-18 18:48:54 +0800

    Update sample-code/linux/driver/mmio

--- sample-code/linux/driver/mmio/mmio.c
+++ sample-code/linux/driver/mmio/mmio.c
@@ -10,15 +10,170 @@
 #include <linux/uaccess.h>
 #include "mmio.h"
 
-mmio_action action;//for read() write()
+//global data 
+mmio_map_range public_addr_range={none_map_state,0};
 
-mmio_map_range map_range={0,0};
+inline void init_map_range(mmio_map_range *map)
+{	AT_LINE
+	map->status = none_map_state;
+}
 
-int check_address_map_range(physical_address phy_addr)
-{
+int unmap_range(mmio_map_range *map)
+{	AT_LINE
+	if( (map->status&none_map_state)==none_map_state) return 0;// bit 0 =0 none map 
+	
+	map->status = none_map_state;
+	iounmap(map->base_addr);
 
 	return 0;
 }
+
+int map_range(physical_address phy_addr,size_t size,mmio_map_range *map)
+{	AT_LINE
+	
+	map->phy_addr = phy_addr & ADDRESS_PAGE_ALIGN;//change map->phy_addr ~(0xFFF)
+	map->offset = phy_addr & ADDRESS_OFFSET_KEEP;//0xFFF
+
+	if(size % MMIO_MAP_PAGE >0) //Not a whole memory page
+	{
+		map->size = (size & ADDRESS_PAGE_ALIGN) + MMIO_MAP_PAGE;//At least one page
+	}
+
+	map->base_addr=ioremap(map->phy_addr, map->size);// save ioremap address to map->base_addr and for iounmap(map->base_addr);
+
+	map->address=map->base_addr+map->offset;//set and save map->address point to phy_addr
+	map->status =in_map_state;
+
+	return 0;// case none map return 
+}
+
+int build_map_range(physical_address phy_addr,size_t size,mmio_map_range *map) 
+{	AT_LINE
+	if( (map->status & none_map_state) == none_map_state )// bit 0 =0 none map 
+	{
+		map_range(phy_addr,size,map);
+		return 0;// case none map return 
+	}
+	//else //if(map->status&in_map_state==in_map_state) //bit 0 =1 in map state
+	
+	if( phy_addr >= map->phy_addr && phy_addr+size <= map->phy_addr + map->size )//set and save map->address point to phy_addr
+	{
+		map->address=map->base_addr+(phy_addr -  map->phy_addr);//offset = phy_addr -  map->phy_addr 
+		return 0;//phy_addr in current map range
+	}
+
+	//else out off mmio map range 
+	unmap_range(map);
+	map_range(phy_addr,size,map);
+	
+	return 0;
+}
+
+int do_mmio_action_read(volatile void *address,mmio_action * action)
+{	AT_LINE
+	if(action->action <= mmio_read_action)	//mmio_read_action
+	{
+		action->data = *(mmio_data *) address;
+		switch(action->action)
+		{
+			case mmio_byte_read  :
+				action->data &=0xFF;
+				break;
+			case mmio_word_read  :
+				action->data &=0xFFFF;
+				break;
+			case mmio_dword_read :
+				break;
+			default:
+		}
+		return 0;
+	}
+	return -1;//do nothing
+}
+
+int do_mmio_action_write(volatile void *address,mmio_action * action)
+{	AT_LINE
+	if(action->action >= mmio_byte_write && action->action <= mmio_write_action)	//mmio_write_action
+	{
+		switch(action->action)
+		{
+			case mmio_byte_write  :
+				* (UINT8 *) address = (UINT8) action->data;
+				break;
+			case mmio_word_write  :
+				* (UINT16 *) address = (UINT16) action->data;
+				break;
+			case mmio_dword_write :
+				* (UINT32 *) address = (UINT32) action->data;
+				break;
+			default:
+				* (UINT32 *) address = (UINT32) action->data;
+		}
+		return 0;	
+	}
+	return -1;//do nothing
+}
+
+int do_mmio_action_and(volatile void *address,mmio_action * action)
+{	AT_LINE
+	if(action->action >= mmio_byte_and && action->action <= mmio_and_action)	//mmio_and_action
+	{
+		switch(action->action)
+		{
+			case mmio_byte_and  :
+				* (UINT8 *) address &= (UINT8) action->data;
+				break;
+			case mmio_word_and  :
+				* (UINT16 *) address &= (UINT16) action->data;
+				break;
+			case mmio_dword_and :
+				* (UINT32 *) address &= (UINT32) action->data;
+				break;
+			default:
+				* (UINT32 *) address &= (UINT32) action->data;
+		}
+		return 0;	
+	}
+
+	return -1;//do nothing
+}
+
+int do_mmio_action_or(volatile void *address,mmio_action * action)
+{	AT_LINE
+	if(action->action >= mmio_byte_or && action->action <= mmio_or_action)	//mmio_or_action
+	{
+		switch(action->action)
+		{
+			case mmio_byte_or  :
+				* (UINT8 *) address |= (UINT8) action->data;
+				break;
+			case mmio_word_or  :
+				* (UINT16 *) address |= (UINT16) action->data;
+				break;
+			case mmio_dword_or :
+				* (UINT32 *) address |= (UINT32) action->data;
+				break;
+			default:
+				* (UINT32 *) address |= (UINT32) action->data;
+		}
+		return 0;	
+	}
+
+	return -1;//do nothing
+}
+
+int do_mmio_action(volatile void *address,mmio_action * action)
+{	AT_LINE
+	if(action->action >= mmio_byte_read && action->action <= mmio_or_action)	//from mmio_byte_read to mmio_or_action
+	{
+		if(0==do_mmio_action_read(address,action)) return 0;
+		if(0==do_mmio_action_write(address,action)) return 0;
+		if(0==do_mmio_action_and(address,action)) return 0;
+		if(0==do_mmio_action_or(address,action)) return 0;
+	}
+	return -1;
+}
+
 
 static int mmio_open(struct inode *inode, struct file *filp)
 {  
@@ -26,19 +181,27 @@ static int mmio_open(struct inode *inode, struct file *filp)
 	return 0;
 }
 
+
 static ssize_t mmio_read (struct file *file, char __user *buf, size_t size, loff_t *offset)
 {
-	int ret;	AT_LINE
-	ret = copy_from_user(&action, buf, sizeof(mmio_action));
+	int ret;	
+	mmio_action action;
+	AT_LINE;
+	ret = copy_from_user(&action, buf, sizeof(mmio_action));//copy mmio_action to action
+	build_map_range(action.phy_addr,MMIO_MAP_PAGE,& public_addr_range);
+	action.status=do_mmio_action(public_addr_range.address,&action);
 	ret = copy_to_user(buf, &action, sizeof(mmio_action));
 	return 0;
 }
 
 static ssize_t mmio_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
 {  
-
-	int ret;AT_LINE
+	int ret;
+	mmio_action action;
+	AT_LINE;
 	ret = copy_from_user(&action, buf, sizeof(mmio_action));
+	build_map_range(action.phy_addr,MMIO_MAP_PAGE,& public_addr_range);
+	action.status=do_mmio_action(public_addr_range.address,&action);
 	ret = copy_to_user((char *)buf,&action, sizeof(mmio_action));
    return 0;
 }
--- sample-code/linux/driver/mmio/mmio.h
+++ sample-code/linux/driver/mmio/mmio.h
@@ -13,17 +13,36 @@
 #endif
 
 //mmio code 
-#define MMIO_MAP_SIZE  (4096) //one address map 4KB(0x1000) one page for mmio_action access
+#define MMIO_MAP_PAGE  (4096) //one address map 4KB(0x1000) one page for mmio_action access
 
-typedef unsigned long long physical_address; //64 bit address 
-typedef unsigned long long mmio_data; //64 bit data
+typedef unsigned char   UINT8; //byte data
+typedef unsigned short  UINT16; //word data
+typedef unsigned int    UINT32; //dword data
+
+typedef unsigned int  physical_address; //32 bit address 
+typedef unsigned int  mmio_data; //32 bit data
+
+#define ADDRESS_OFFSET_KEEP  0xFFF
+#define ADDRESS_PAGE_ALIGN   0xFFFFF000
+
+//note : do not change mmio_action_enum define order do_mmio_action() used 
+//  if(action->action > mmio_read_action && action->action <= mmio_write_action)    //mmio_write_action
+//  if(action->action <= mmio_read_action)  //mmio_read_action
+//  if(action->action > mmio_write_action && action->action <= mmio_and_action) //mmio_and_action
 
 enum mmio_action_enum { 
-    mmio_byte_read=1, mmio_word_read,  mmio_dword_read, mmio_qword_read, // read action : [mmio_address] to data 
-    mmio_byte_write,  mmio_word_write, mmio_dword_write,mmio_qword_write,// write action: data to [mmio_address] 
-    mmio_byte_and,    mmio_word_and,   mmio_dword_and,  mmio_qword_and,// write and action: data to [mmio_address] 
-    mmio_byte_or,     mmio_word_or,    mmio_dword_or,   mmio_qword_or,// write or action: data to [mmio_address] 
+    mmio_byte_read=1, mmio_word_read,  mmio_dword_read, mmio_read_action,// read action : [mmio_address] to data 
+    mmio_byte_write,  mmio_word_write, mmio_dword_write,mmio_write_action,// write action: data to [mmio_address] 
+    mmio_byte_and,    mmio_word_and,   mmio_dword_and,  mmio_and_action,// write and action: data to [mmio_address] 
+    mmio_byte_or,     mmio_word_or,    mmio_dword_or,   mmio_or_action,// write or action: data to [mmio_address] 
 };
+
+enum mmio_map_status { 
+    none_map_state=0,//bit0=0
+    in_map_state=1,//bit 0=1
+    map_change_state=2,//bit 1=1
+};
+
 
 typedef struct mmio_action_struct
 {
@@ -35,8 +54,12 @@ typedef struct mmio_action_struct
 
 typedef struct mmio_map_range_struct
 {
-    physical_address phy_addr;//4K alignment
+    int status;
     size_t size; //4K units
+    physical_address phy_addr;//4K alignment
+    int offset;
+    volatile void *base_addr;//4K base address from ioremap
+    volatile void *address;//base_addr + offset
 }mmio_map_range;
 
 #endif //MMIO_H  
