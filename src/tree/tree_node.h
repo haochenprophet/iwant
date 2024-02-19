5a3b17bf4224a297faeccf9f60e307c3d908a913
hunter <hao__chen@sina.com>
2024-02-19 17:51:50 +0800

    Add sub_node_count(),add_sub_node(tree_node* node) to class tree_node

--- src/tree/tree_node.h
+++ src/tree/tree_node.h
@@ -14,10 +14,15 @@ namespace n_tree_node {
 	public:
 		tree_sub_node sub_node;
 		tree_data data;
+	public:
+		size_t sub_node_count() { return this->sub_node.size(); }
+		void add_sub_node(tree_node* node) { this->sub_node.push_back((void *)node); }
 	};
 
 	class Ctree_node :public Object
 	{
+	public:
+		tree_node node;
 	public:
 		Ctree_node();
 		~Ctree_node();
