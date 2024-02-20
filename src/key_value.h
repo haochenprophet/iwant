dfbe66c1b2506c7eeaedb1ac9bb95d81a5050f78
hunter <hao__chen@sina.com>
2024-02-20 10:46:02 +0800

    Add class key_value,key_value_list list;

--- src/key_value.h
+++ src/key_value.h
@@ -4,8 +4,23 @@
 #include "object.h"
 
 namespace n_key_value {
+
+	class key_value
+	{
+	public:
+		void * key;//point key .UUID ,MD5 SHA key ..or  Udata
+		void * value;// point value or value buffer address
+		size_t size;// value size
+	public:
+
+	};
+
+	typedef std::list<key_value *> key_value_list;
+
 	class Ckey_value :public Object
 	{
+	public:
+		key_value_list list;
 	public:
 		Ckey_value();
 		~Ckey_value();
