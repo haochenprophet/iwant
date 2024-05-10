1a4af1af6278a838fe689386fba7e6e883df2430
hunter <hao__chen@sina.com>
2024-05-10 17:55:59 +0800

    Update class Cbuild_makefile

--- src/build/build_makefile.cpp
+++ src/build/build_makefile.cpp
@@ -13,7 +13,36 @@ Cbuild_makefile::Cbuild_makefile()
 
 Cbuild_makefile::~Cbuild_makefile()
 {
+	this->block_list.clear();
+}
 
+void Cbuild_makefile::add_define(string s_def)
+{
+	this->define_list.push_back(s_def);
+}
+
+void Cbuild_makefile::add_block(Cmakefile_block block)
+{
+	this->block_list.push_back(block);
+}
+
+int  Cbuild_makefile::display()
+{
+	if (this->block_list.empty()) return -1;
+
+	list_makefile_define::iterator it_def;
+	for (it_def = this->define_list.begin(); it_def != this->define_list.end(); ++it_def)
+	{
+		std::cout << (*it_def) << endl;
+	}
+	std::cout << endl;
+	list_makefile_block::iterator it;
+	for (it = this->block_list.begin(); it != this->block_list.end(); ++it)
+	{
+		std::cout <<(*it).targets << " : "<< (*it).dependents << endl << "\t" << (*it).commands <<endl;
+	}
+
+	return 0;
 }
 
 #ifndef BUILD_MAKEFILE_TEST
@@ -25,6 +54,33 @@ Cbuild_makefile::~Cbuild_makefile()
 int main(int argc, char *argv[])
 {
 	WHERE_I;
+
+	Cbuild_makefile build_makefile;
+
+	string s_def;
+	Cmakefile_block block;
+
+	//tag
+	s_def = "MK_TAG = database";
+	build_makefile.add_define(s_def);
+
+	//cflag
+	s_def = "CFLAGS += -std=gnu++11";
+	build_makefile.add_define(s_def);
+
+	//all
+	block.targets = "all";
+	block.dependents = "";
+	block.commands = "g++ $(CFLAGS)";
+	build_makefile.add_block(block);
+
+	//claen
+	block.targets = "clean";
+	block.dependents = "";
+	block.commands = "rm -f $(MK_TAG) *.o *~ .*.swp";
+	build_makefile.add_block(block);
+	
+	build_makefile.display();
 
 	return 0;
 }
--- src/build/build_makefile.h
+++ src/build/build_makefile.h
@@ -4,13 +4,32 @@
 #include "object.h"
 
 namespace n_build_makefile {
+
+	class Cmakefile_block
+	{
+	public:
+		string targets;
+		string dependents;
+		string commands;
+	};
+
+	typedef std::list<Cmakefile_block> list_makefile_block;
+	typedef std::list<string> list_makefile_define;
+
 	class Cbuild_makefile :public Object
 	{
+	public:
+		list_makefile_define define_list;
+ 		list_makefile_block block_list;
 	public:
 		Cbuild_makefile();
 		~Cbuild_makefile();
 		int my_init(void *p=nullptr);
 		size_t my_size() {return sizeof(Cbuild_makefile); }
+	public:
+		void add_define(string s_def);
+		void add_block(Cmakefile_block block);
+		int  display();
 	};
 }
 
