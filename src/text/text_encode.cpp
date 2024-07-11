7fa78a4b22f1d3890516c7b1809ac7b8d4677db2
hunter <hao__chen@sina.com>
2024-07-11 21:10:08 +0800

    Update class Ctext_encode

--- src/text/text_encode.cpp
+++ src/text/text_encode.cpp
@@ -1,8 +1,52 @@
 #include "text_encode.h"
+
+TextEncode text_encode_tab[] =
+{
+	{TextEncodeID::BOM_UTF_8,(char*)"BOM_UTF-8"},
+	{TextEncodeID::UTF_16_LE,(char*)"UTF-16 LE"},
+	{TextEncodeID::UTF_16_BE,(char*)"UTF-16 BE"},
+	{TextEncodeID::UTF_8,(char*)"UTF-8"},
+	{TextEncodeID::ASCII,(char*)"ASCII"},
+	{TextEncodeID::GB18030,(char*)"GB18030"},
+};
+
+#define TEXT_ENCODE_COUNT (sizeof(text_encode_tab)/sizeof(TextEncode))
+
+bool TextEncode::isMe(TextEncodeID id)
+{
+	return (this->id == id);
+}
+
+bool TextEncode::isMe(int id)
+{
+	return (this->id == (TextEncodeID)id);
+}
+
+char * TextEncode:: my_name()
+{
+	return this->name;
+}
+
+int TextEncode::set_name(TextEncodeID id)
+{
+	int n = 0;
+	for (; n < TEXT_ENCODE_COUNT; n++)
+	{
+		if (text_encode_tab[n].isMe(id))
+		{
+			this->name = text_encode_tab[n].my_name();
+			return 0;
+		}
+	}
+	return -1;
+}
+
 int Ctext_encode::my_init(void *p)
 {
 	this->name = "Ctext_encode";
 	this->alias = "text_encode";
+	this->encode.id = TextEncodeID::UTF_8;
+	this->encode.name = (char*)"UTF-8";
 	return 0;
 }
 
--- src/text/text_encode.h
+++ src/text/text_encode.h
@@ -4,8 +4,33 @@
 #include "object/object.h"
 
 namespace n_text_encode {
+
+	enum class TextEncodeID
+	{
+		ASCII = 0,
+		UTF_8 = 1,
+		GB18030 = 2,
+		BOM_UTF_8 = 0xBFBBEF,
+		UTF_16_LE = 0xFEFF,
+		UTF_16_BE = 0xFFFE,
+	};
+
+	class TextEncode
+	{
+	public:
+		TextEncodeID    id;
+		char * name;
+	public :
+		bool isMe(TextEncodeID id);
+		bool isMe(int id);
+		int set_name(TextEncodeID id);
+		char * my_name();
+	};
+
 	class Ctext_encode :public Object
 	{
+	public:
+		TextEncode encode;
 	public:
 		Ctext_encode();
 		~Ctext_encode();
